ce24e31f94dc3df115625bfd55b78116cfee90d7
Devendra Lodhi <devendralodhi176@gmail.com>
2022-12-05 13:24:38 +0530

    UPDATE: Updated function to show active or not on change tab

--- example/consumers.py
+++ example/consumers.py
@@ -1,3 +1,5 @@
+import json
+
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
@@ -18,7 +20,35 @@ class UserConsumer(WebsocketConsumer):
             self.group_name,
             self.channel_name,
         )
-    
+
+    def receive(self, text_data=None):
+        if not json.loads(text_data).get('active'):
+            async_to_sync(self.channel_layer.group_send)(
+                    "users",
+                    {
+                        "type": "update_user",
+                        "value": json.dumps(
+                                {
+                                    'username': self.user.username,
+                                    'is_logged_in': False,
+                                }
+                            )
+                    }
+                )
+        if json.loads(text_data).get('active'):
+            async_to_sync(self.channel_layer.group_send)(
+                "users",
+                {
+                    "type": "update_user",
+                    "value": json.dumps(
+                            {
+                                'username': self.user.username,
+                                'is_logged_in': True,
+                            }
+                        )
+                }
+            )
+
     def update_user(self, event):
         data = event.get('value')
         self.send(text_data=data)
